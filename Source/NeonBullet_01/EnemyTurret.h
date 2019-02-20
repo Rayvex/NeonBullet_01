3bd86e4c8542d6ad384fea6b61379cf65fb2bb6a
Odin Giske <odin.giske@gmail.com>
2019-02-20 18:57:46 +0100

    Added turrets with tracking

---
+++ Source/NeonBullet_01/EnemyTurret.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EnemyTurret.generated.h"
+
+UCLASS()
+class NEONBULLET_01_API AEnemyTurret : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEnemyTurret();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	FVector MoveDirection = FVector(0.f, 0.f, 0.f);
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+		float TurnDelayMax = 6.f;
+
+	/*How long min till turn towards player*/
+	UPROPERTY(EditAnywhere)
+		float TurnDelayMin = 1.f;
+
+	float CurrentTurnDelay = 2.f;
+
+
+};
